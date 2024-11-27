4f1f3da4187caf6ed16838c932401be3fcb6da62
liciemw <liciemumbi@gmail.com>
2024-11-27 04:22:43 -0500

    initial commit

--- hardhat.config.js
+++ hardhat.config.js
@@ -13,7 +13,7 @@ require("hardhat-deploy")
 const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || ""
 const SEPOLIA_RPC_URL =
     process.env.SEPOLIA_RPC_URL ||
-    "https://eth-sepolia.g.alchemy.com/v2/YOUR-API-KEY"
+    "https://eth-sepolia.g.alchemy.com/v2/VLVYka1fJcAf5mJc1doN2ryyweSTp1HV"
 const PRIVATE_KEY =
     process.env.PRIVATE_KEY ||
     "0x11ee3108a03081fe260ecdc106554d09d9d1209bcafd46942b10e02943effc4a"
@@ -24,7 +24,7 @@ module.exports = {
     networks: {
         hardhat: {
             chainId: 31337,
-            // gasPrice: 130000000000,
+            // gasPrice: 20000000000,
         },
         sepolia: {
             url: SEPOLIA_RPC_URL,
